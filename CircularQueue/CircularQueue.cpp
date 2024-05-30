74cc34c8fdf32dbde080ab266599610b2ba1cece
Mihaly24 <fairuzk20@gmail.com>
2024-05-30 18:15:22 +0700

    Menambahkan isi void remove

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -42,6 +42,10 @@ public:
         }
         cout << "The element deleted from the queue list: " << queue_array[FRONT] << "\n";
 
+        if (FRONT == REAR) {
+            FRONT = -1;
+            REAR = -1;
+        }
     }
 };
 
