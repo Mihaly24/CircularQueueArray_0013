3fd05fbecaed455024869137a26a4ebacf0ef796
Mihaly24 <fairuzk20@gmail.com>
2024-05-30 18:27:33 +0700

    Menambahkan isi void display

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -56,6 +56,15 @@ public:
 
     void display() {
         int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        if (FRONT == -1) {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are... \n";
+
     }
 };
 
