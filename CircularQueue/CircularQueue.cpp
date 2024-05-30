089100e25ea765ebfade50d649803f989cb2cd05
Mihaly24 <fairuzk20@gmail.com>
2024-05-30 18:15:34 +0700

    Melengkapi isi void remove

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -46,6 +46,12 @@ public:
             FRONT = -1;
             REAR = -1;
         }
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT - 1;
+        }
     }
 };
 
