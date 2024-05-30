13fdea35851c8a2a7443199c60e4416767759492
Mihaly24 <fairuzk20@gmail.com>
2024-05-30 18:40:48 +0700

    Menambahkan pilihan

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -94,5 +94,16 @@ int main()
     Queue q;
     char ch;
 
-
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter yourr choice (1-4): ";
+            cin << ch;
+            cout << endl;
+        }
+    }
 }
